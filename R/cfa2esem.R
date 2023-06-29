31ae8dac716f145c2b3adc78496d1bb20b741335
Ali Zia-Tohidi <towhidi@ut.ac.ir>
2023-06-29 16:27:25 +0330

    Add an example

--- R/cfa2esem.R
+++ R/cfa2esem.R
@@ -1,12 +1,31 @@
 #' Generate ESEM syntax from a CFA fit
 #'
 #' \code{cfa2esem} takes a CFA fit from lavaan and returns its corresponding ESEM syntax.
-#' @param cfa.fit
+#' @param cfa.fit An fitted model of class \code{lavaan}
 #'
-#' @return
+#' @return A string syntax for its corresponding ESEM model
 #' @export
 #'
 #' @examples
+#' # fit the CFA model
+#'
+#' cfa.mod <- ' visual =~ x1 + x2 + x3
+#' textual =~ x4 + x5 + x6
+#' speed =~ x7 + x8 + x9'
+#'
+#' cfa.fit <- lavaan::cfa(cfa.mod, data = HolzingerSwineford1939, estimator = 'MLR')
+#'
+#' summary(cfa.fit, std = T, fit = T)
+#'
+#'
+#' # fit the ESEM model
+#'
+#' esem.mod <- cfa2esem(cfa.fit)
+#'
+#' esem.fit <- cfa(esem.mod, data = HolzingerSwineford1939, estimator = 'MLR')
+#'
+#' summary(esem.fit, std = T, fit = T)
+#'
 cfa2esem <- function(cfa.fit) {
   syntax.esem <- paste0(
     paste0(
