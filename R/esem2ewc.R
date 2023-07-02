8f2a06848c529b5b7a78eee0e1adb1faecb6bb30
Ali Zia-Tohidi <towhidi@ut.ac.ir>
2023-07-02 23:08:18 +0330

    - It now allows for set ESEM models, i.e., with multiple EFA blocks - Several warnings and stop messages were added - A warning message to hint that the model must be fitted the arguement 'std.lv' set to 'TRUE'.

--- R/esem2ewc.R
+++ R/esem2ewc.R
@@ -10,22 +10,50 @@
 #' @export
 #'
 #' @examples
+#'
 esem2ewc <- function(esem.fit, referents){
-  temp <- esemTools::esem_loadings(esem.fit)
-  ovs <- temp[,1]
-  lvs <- colnames(temp)[-1]
-  bs <- temp[,-1]
-  syn <- matrix(nrow = length(ovs), ncol = length(lvs), dimnames = list(c(ovs), c(lvs)))
-  for(i in 1:length(ovs)) {
-    syn[i,] <- paste0('start(', bs[i,], ') * ', ovs[i])
+  nefa <- esem.fit@Model@nefa
+  efa.labels <- names(esem.fit@Model@lv.efa.idx[[1]])
+  efa.lv.length <- lapply(esem.fit@Model@lv.efa.idx[[1]], length)
+  if(class(esem.fit) != 'lavaan') {
+    stop("'esem.fit' must be a fitted ESEM model of class 'lavaan'")
   }
-  for(i in 1:length(lvs)) {
-    syn[ovs==referents[[i]],-i] <- paste0(bs[ovs==referents[[i]],-i], ' * ',referents[[i]])
+  if(esem.fit@Model@nefa == 0) {
+    stop("This is not an ESEM model.")
   }
-  syn.2 <- c()
-  for(i in 1:length(lvs)) {
-    syn.2[i] <- paste(syn[,i], collapse = ' + ')
+  if(is.list(referents[[1]]) == FALSE) {
+    referents <- list(referents)
   }
-  syntax <- paste(lvs, ' =~ ', syn.2, collapse = ' \n ')
-  return(syntax)
+  if(nefa != length(referents)) {
+    stop(paste0("Your model has ", nefa, " EFA blocks; Supply ", nefa, " lists of referents."))
+  }
+  if(setequal(unlist(lapply(referents, length)), unlist(efa.lv.length)) == FALSE)
+     {stop("The number of referent variables you specified is not identical to the number of EFA factors in the model.")}
+  f.loadings <- list()
+  ovs <- list()
+  lvs <- list()
+  bs <- list()
+  syn <- list()
+  syntax <- list()
+  for(i in 1:nefa) {
+    f.loadings[[i]] <- esemTools::esem_loadings(esem.fit, efa.block = efa.labels[i])
+    ovs[[i]] <- f.loadings[[i]][,1]
+    lvs[[i]] <- colnames(f.loadings[[i]])[-1]
+    bs[[i]] <- f.loadings[[i]][,-1]
+    syn[[i]] <- matrix(nrow = length(ovs[[i]]), ncol = length(lvs[[i]]), dimnames = list(c(ovs[[i]]), c(lvs[[i]])))
+    for(j in 1:length(ovs[[i]])) {
+      syn[[i]][j,] <- paste0('start(', bs[[i]][j,], ') * ', ovs[[i]][j])
+    }
+    for(j in 1:length(lvs[[i]])) {
+      syn[[i]][ovs[[i]]==referents[[i]][[j]],-j] <- paste0(bs[[i]][ovs[[i]]==referents[[i]][[j]],-j], ' * ',referents[[i]][[j]])
+    }
+    syn.2 <- as.list(1:nefa)
+    for(j in 1:length(lvs[[i]])) {
+      syn.2[[i]][j] <- paste(syn[[i]][,j], collapse = ' + ')
+    }
+    syntax[[i]] <- paste(lvs[[i]], ' =~ ', syn.2[[i]], collapse = ' \n ')
+  }
+  ewc.syn <- paste(syntax, collapse = ' \n ')
+  warning("When fitting the model, make sure that 'std.lv' is set to 'TRUE'.")
+  return(ewc.syn)
 }
